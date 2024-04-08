c5432aabd87acf8dd4a372b14cf98c3de759cc67
saygac <saygac@cip.ifi.lmu.de>
2024-04-08 11:52:35 +0300

    finish board

--- board.cpp
+++ board.cpp
@@ -49,6 +49,7 @@ void Board::deleteLine(int pY) {
     }
 }
 
+// Find the lines to delete and use the deleteLine function to delete them, if any
 void Board::deletePossibleLines() {
     for (int j = 0; j < BOARD_HEIGHT; j++) {
         int i = 0;
@@ -61,3 +62,36 @@ void Board::deletePossibleLines() {
     }
 }
 
+// Check if the coordinate is free
+bool Board::isFreeBlock(int x, int y) {
+    return board[x][y] == POS_FREE;
+}
+
+// Get positions in pixels for y and x coordinates.
+int Board::getXPosInPixels(int pos) {
+    return ((BOARD_POSITION - (BlOCK_SIZE * (BOARD_WIDTH / 2)) + (pos * BlOCK_SIZE)));
+}
+
+int Board::getYPosInPixels(int pos) {
+    return ((GScreenHeigth - (BlOCK_SIZE * BOARD_HEIGHT)) + (pos * BlOCK_SIZE));
+}
+
+// Check whether a move is possible
+bool Board::isPossibleMovement(int x, int y, int piece, int rotation) {
+    // Checking for collision with already stored pieces and the limits
+
+    for (int i1 = x, int i2 = 0; i1 < x + PIECE_BLOCKS; i1++, i2++) {
+        for (int j1 = y, int j2 = 0; j1 < y + PIECE_BLOCKS; j1++, j2++) {
+            if (i1 < 0 || i1 > BOARD_WIDTH - 1 || j1 > BOARD_HEIGHT - 1) {
+                if (gPieces->getBlockType(piece, rotation,i2, j2)) return 0;  
+            }
+
+            if (j1 >= 1) {
+                if ((gPieces->getBlockType(piece, rotation, x, y) != 0) && !isFreeBlock(i1,j1)) return false;
+            }
+        }
+    }
+
+    // No collision
+    return true;
+}
